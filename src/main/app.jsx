e2f343068cef43a02fd973485522c6b2e55bcc96
LuanDantas <kidinoop@hotmail.com>
2018-08-12 22:16:24 -0300

    Create about component.

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -2,11 +2,16 @@
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
+// Default imports
 import React from 'react'
+
+// Components & Pages imports
 import Todo from '../to-do/to-do'
+import About from '../about/about'
 
 export default props => (
     <div className='container'>
         <Todo />
+        <About />
     </div>
 )
