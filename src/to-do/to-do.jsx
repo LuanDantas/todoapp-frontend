be99ca9422279147eccc0548ce0077763ec73747
LuanDantas <kidinoop@hotmail.com>
2018-08-13 08:36:53 -0300

    Create 2 new components 'to-do-form' and 'to-do-list', and style form on to-do-form component.

--- src/to-do/to-do.jsx
+++ src/to-do/to-do.jsx
@@ -1,11 +1,18 @@
+// Default imports
 import React, { Component } from 'react'
 import PageHeader from '../template/page-header'
+
+// Components imports
+import TodoForm from './to-do-form'
+import TodoList from './to-do-list'
 
 export default class Todo extends Component {
     render() {
         return(
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+                <TodoForm />
+                <TodoList />
             </div>
         )
     }
